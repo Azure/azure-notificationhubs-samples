8c4a815074c2e9c3717a20dee52ea37bbf206ffd
Piyush Joshi <piyushjo@microsoft.com>
2015-01-27 14:34:23 -0800

    Adding enterprise guidance sample

---
+++ EnteprisePush/EnterprisePushBackendSystem/EnterprisePushBackendSystem/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+using System;
+
+namespace EnterprisePushBackendSystem
+{
+    class Program
+    {
+        const string sampleTopic = "HRSystem";
+        
+        // See - http://azure.microsoft.com/en-us/documentation/articles/service-bus-dotnet-how-to-use-topics-subscriptions/ for details
+        //  for details on Service Bus Topic/Subscriptions based messaging
+
+        static void Main(string[] args)
+        {
+            string connectionString =
+                CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            // Create the topic where we will send notifications
+            CreateTopic(connectionString);
+
+            // Send message
+            SendMessage(connectionString);
+        }
+
+        public static void CreateTopic(string connectionString)
+        {
+            // Create the topic if it does not exist already
+
+            var namespaceManager =
+                NamespaceManager.CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.TopicExists(sampleTopic))
+            {
+                namespaceManager.CreateTopic(sampleTopic);
+            }
+        }
+
+        public static void SendMessage(string connectionString)
+        {
+            TopicClient client =
+                TopicClient.CreateFromConnectionString(connectionString, sampleTopic);
+
+            // Sends random messages every 10 seconds to the topic
+            string[] messages = 
+            {
+                "Employee Id '{0}' has joined.",
+                "Employee Id '{0}' has left.",
+                "Employee Id '{0}' has switched to a different team."
+            };
+
+            while (true)
+            {
+                Random rnd = new Random();
+                string employeeId = rnd.Next(10000, 99999).ToString();
+                string notification = String.Format(messages[rnd.Next(0,messages.Length)], employeeId);
+
+                // Send Notification
+                BrokeredMessage message = new BrokeredMessage(notification);
+                client.Send(message);
+
+                Console.WriteLine("{0} Message sent - '{1}'", DateTime.Now, notification);
+
+                System.Threading.Thread.Sleep(new TimeSpan(0, 0, 10));
+            }
+        }
+    }
+}
---
+++ EnteprisePush/ReceiveAndSendNotification/ReceiveAndSendNotification/Program.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.ServiceBus.Notifications;
+using Microsoft.WindowsAzure;
+using System;
+
+namespace ReceiveAndSendNotification
+{
+    class Program
+    {
+        const string sampleTopic = "HRSystem";
+        const string sampleSubscription = "HRSystemSubscription";
+        private static NotificationHubClient hub;
+
+        // See - http://azure.microsoft.com/en-us/documentation/articles/service-bus-dotnet-how-to-use-topics-subscriptions/ for details
+        //  for details on Service Bus Topic/Subscriptions based messaging
+
+        static void Main(string[] args)
+        {
+            string connectionString =
+                     CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            // Create the subscription which will receive messages
+            CreateSubscription(connectionString);   
+
+            // Receive message
+            ReceiveMessageAndSendNotification(connectionString);
+        }
+
+        static void CreateSubscription(string connectionString)
+        {
+            // Create the subscription if it does not exist already
+            var namespaceManager =
+                NamespaceManager.CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.SubscriptionExists(sampleTopic, sampleSubscription))
+            {
+                namespaceManager.CreateSubscription(sampleTopic, sampleSubscription);
+            }
+        }
+
+        static void ReceiveMessageAndSendNotification(string connectionString)
+        {
+            // Initialize the Notification Hub
+            string hubConnectionString = CloudConfigurationManager.GetSetting
+                    ("Microsoft.NotificationHub.ConnectionString");
+            hub = NotificationHubClient.CreateClientFromConnectionString
+                    (hubConnectionString, "[hubName]");
+            
+            SubscriptionClient Client =
+                SubscriptionClient.CreateFromConnectionString
+                        (connectionString, sampleTopic, sampleSubscription);
+
+            Client.Receive();
+
+            // Continuously process messages received from the subscription 
+            while (true)
+            {
+                BrokeredMessage message = Client.Receive();
+                var toastMessage = @"<toast><visual><binding template=""ToastText01""><text id=""1"">{messagepayload}</text></binding></visual></toast>";
+
+                if (message != null)
+                {
+                    try
+                    {
+                        Console.WriteLine(message.MessageId);
+                        Console.WriteLine(message.SequenceNumber);
+                        string messageBody = message.GetBody<string>();
+                        Console.WriteLine("Body: " + messageBody + "\n");
+
+                        toastMessage = toastMessage.Replace("{messagepayload}", messageBody);
+                        SendNotificationAsync(toastMessage);
+
+                        // Remove message from subscription
+                        message.Complete();
+                    }
+                    catch (Exception)
+                    {
+                        // Indicate a problem, unlock message in subscription
+                        message.Abandon();
+                    }
+                }
+            } 
+        }
+        static async void SendNotificationAsync(string message)
+        {
+            await hub.SendWindowsNativeNotificationAsync(message);
+        }
+    }
+}

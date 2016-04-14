8ef770036f43ece8a7dd4c6618ae57772d0a1105
wesmc7777 <wesmc@microsoft.com>
2016-04-14 16:16:28 -0400

    removed depreciated API for sending notification with REST

--- Android/GetStarted/app/src/main/java/com/example/microsoft/getstarted/MainActivity.java
+++ Android/GetStarted/app/src/main/java/com/example/microsoft/getstarted/MainActivity.java
@@ -12,6 +12,19 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import android.util.Base64;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     public static Boolean isVisible = false;
     private static final String TAG = "MainActivity";
+    
+    private String HubEndpoint = null;
+    private String HubSasKeyName = null;
+    private String HubSasKeyValue = null;   
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +82,161 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Example code from http://msdn.microsoft.com/library/azure/dn495627.aspx
+     * to parse the connection string so a SaS authentication token can be
+     * constructed.
+     *
+     * @param connectionString This must be the DefaultFullSharedAccess connection
+     *                         string for this example.
+     */
+    private void ParseConnectionString(String connectionString)
+    {
+        String[] parts = connectionString.split(";");
+        if (parts.length != 3)
+            throw new RuntimeException("Error parsing connection string: "
+                    + connectionString);
+
+        for (int i = 0; i < parts.length; i++) {
+            if (parts[i].startsWith("Endpoint")) {
+                this.HubEndpoint = "https" + parts[i].substring(11);
+            } else if (parts[i].startsWith("SharedAccessKeyName")) {
+                this.HubSasKeyName = parts[i].substring(20);
+            } else if (parts[i].startsWith("SharedAccessKey")) {
+                this.HubSasKeyValue = parts[i].substring(16);
+            }
+        }
+    }
+
+    /**
+     * Example code from http://msdn.microsoft.com/library/azure/dn495627.aspx to
+     * construct a SaS token from the access key to authenticate a request.
+     *
+     * @param uri The unencoded resource URI string for this operation. The resource
+     *            URI is the full URI of the Service Bus resource to which access is
+     *            claimed. For example,
+     *            "http://<namespace>.servicebus.windows.net/<hubName>"
+     */
+    private String generateSasToken(String uri) {
+
+        String targetUri;
+        String token = null;
+        try {
+            targetUri = URLEncoder
+                    .encode(uri.toString().toLowerCase(), "UTF-8")
+                    .toLowerCase();
+
+            long expiresOnDate = System.currentTimeMillis();
+            int expiresInMins = 60; // 1 hour
+            expiresOnDate += expiresInMins * 60 * 1000;
+            long expires = expiresOnDate / 1000;
+            String toSign = targetUri + "\n" + expires;
+
+            // Get an hmac_sha1 key from the raw key bytes
+            byte[] keyBytes = HubSasKeyValue.getBytes("UTF-8");
+            SecretKeySpec signingKey = new SecretKeySpec(keyBytes, "HmacSHA256");
+
+            // Get an hmac_sha1 Mac instance and initialize with the signing key
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(signingKey);
+
+            // Compute the hmac on input data bytes
+            byte[] rawHmac = mac.doFinal(toSign.getBytes("UTF-8"));
+
+            // Using android.util.Base64 for Android Studio instead of
+            // Apache commons codec
+            String signature = URLEncoder.encode(
+                    Base64.encodeToString(rawHmac, Base64.NO_WRAP).toString(), "UTF-8");
+
+            // Construct authorization string
+            token = "SharedAccessSignature sr=" + targetUri + "&sig="
+                    + signature + "&se=" + expires + "&skn=" + HubSasKeyName;
+        } catch (Exception e) {
+            if (isVisible) {
+                ToastNotify("Exception Generating SaS : " + e.getMessage().toString());
+            }
+        }
+
+        return token;
+    }
+
+    /**
+     * Send Notification button click handler. This method parses the
+     * DefaultFullSharedAccess connection string and generates a SaS token. The
+     * token is added to the Authorization header on the POST request to the
+     * notification hub. The text in the editTextNotificationMessage control
+     * is added as the JSON body for the request to add a GCM message to the hub.
+     *
+     * @param v
+     */
+    public void sendNotificationButtonOnClick(View v) {
+        EditText notificationText = (EditText) findViewById(R.id.editTextNotificationMessage);
+        final String json = "{\"data\":{\"message\":\"" + notificationText.getText().toString() + "\"}}";
+
+        new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    // Based on reference documentation...
+                    // http://msdn.microsoft.com/library/azure/dn223273.aspx
+                    ParseConnectionString(NotificationSettings.HubFullAccess);
+                    URL url = new URL(HubEndpoint + NotificationSettings.HubName +
+                            "/messages/?api-version=2015-01");
+
+                    HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+
+                    try {
+                        // POST request
+                        urlConnection.setDoOutput(true);
+
+                        // Authenticate the POST request with the SaS token
+                        urlConnection.setRequestProperty("Authorization", generateSasToken(url.toString()));
+
+                        // Notification format should be GCM
+                        urlConnection.setRequestProperty("ServiceBusNotification-Format", "gcm");
+
+                        // Include any tags
+                        // Example below targets 3 specific tags
+                        // Refer to : https://azure.microsoft.com/en-us/documentation/articles/notification-hubs-routing-tag-expressions/
+                        // urlConnection.setRequestProperty("ServiceBusNotification-Tags", "tag1 || tag2 || tag3");
+
+                        // Send notification message
+                        urlConnection.setFixedLengthStreamingMode(json.length());
+                        OutputStream bodyStream = new BufferedOutputStream(urlConnection.getOutputStream());
+                        bodyStream.write(json.getBytes());
+                        bodyStream.close();
+
+                        // Get reponse
+                        urlConnection.connect();
+                        int responseCode = urlConnection.getResponseCode();
+                        if ((responseCode != 200) && (responseCode != 201)) {
+                            if (isVisible) {
+                                BufferedReader br = new BufferedReader(new InputStreamReader((urlConnection.getErrorStream())));
+                                String line;
+                                StringBuilder builder = new StringBuilder("Send Notification returned " +
+                                        responseCode + " : ")  ;
+                                while ((line = br.readLine()) != null) {
+                                    builder.append(line);
+                                }
+
+                                ToastNotify(builder.toString());
+                            }
+                        }
+                    } finally {
+                        urlConnection.disconnect();
+                    }
+                }
+                catch(Exception e)
+                {
+                    if (isVisible) {
+                        ToastNotify("Exception Sending Notification : " + e.getMessage().toString());
+                    }
+                }
+            }
+        }.start();
+    }
 
     @Override
     protected void onStart() {

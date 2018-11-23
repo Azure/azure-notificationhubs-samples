9ae281c1b4d3ce881ce42de0e23f3f723dc1d1f4
Fedor Kalugin <v-fekalu@microsoft.com>
2018-11-23 17:07:46 +0300

    Moved out onNewToken (onRefreshToken) from deprecated class to MyFirebaseMessagingServiceHandler

--- Android/GetStartedFirebase/app/src/main/java/com/example/microsoft/getstartednh/MyFirebaseMessagingServiceHandler.java
+++ Android/GetStartedFirebase/app/src/main/java/com/example/microsoft/getstartednh/MyFirebaseMessagingServiceHandler.java
@@ -12,7 +12,9 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -20,6 +22,13 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessagingServiceHandler extends FirebaseMessagingService {
     public static final int NOTIFICATION_ID = 1;
     private NotificationManager mNotificationManager;
+
+    @Override
+    public void onNewToken (String token){
+        final String TAG = "FCM registration";
+
+        Log.d(TAG, "New FCM registration token: " + token);
+    }
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
@@ -45,7 +54,7 @@ public class MyFirebaseMessagingServiceHandler extends FirebaseMessagingService 
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(ctx)
+                new NotificationCompat.Builder(ctx, "FCM channel")
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle("Notification Hub Demo")
                         .setStyle(new NotificationCompat.BigTextStyle()

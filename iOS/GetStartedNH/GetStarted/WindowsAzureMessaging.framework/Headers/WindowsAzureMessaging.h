b9e69868f56b0a3c522029886c4242c4401e3a00
Matthew Podwysocki <matthewp@microsoft.com>
2019-04-11 13:13:52 -0400

    chore(iOS): remove old sample apps

--- iOS/GetStartedNH/GetStarted/WindowsAzureMessaging.framework/Headers/WindowsAzureMessaging.h
+++
@@ -1,6 +0,0 @@
-//----------------------------------------------------------------
-//  Copyright (c) Microsoft Corporation. All rights reserved.
-//----------------------------------------------------------------
-
-#import "SBNotificationHub.h"
-#import "SBConnectionString.h"
---
+++ iOS/readme.md
@@ -0,0 +1,3 @@
+# iOS Sample Applications
+
+All of the iOS sample applications have been moved moved to [the Azure Notification Hubs iOS SDK repository](https://github.com/Azure/azure-notificationhubs-ios/tree/master/Samples).

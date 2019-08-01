8574956ff1492a2fa0ef1e9e1ba2b090854ca3cb
John M. Wargo <30843404+jwargo@users.noreply.github.com>
2019-08-01 11:02:42 -0400

    config file

--- Ionic/ionic-4/src/app/config.ts
+++ Ionic/ionic-4/src/app/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-    hubName: 'JMW-Test',
-    hubConnectionString: 'Endpoint=sb://jmwns01.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=dxJb7C8dScCXoAvlHvUo5yfnHYCEs4DNipwkWjw+m0I='
+    hubName: '',
+    hubConnectionString: ''
 };
